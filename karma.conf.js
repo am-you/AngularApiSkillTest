56eae2b73fd112d5df3a4c35f5905332e28233c4
amyou <amine.youssfi10@gmail.com>
2020-06-21 18:03:03 +0200

    deploy to ghp

--- karma.conf.js
+++ karma.conf.js
@@ -16,11 +16,11 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-troiscentsoixante-skill-test'),
+      dir: require('path').join(__dirname, './coverage/AngularApiSkillTest'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'html'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
